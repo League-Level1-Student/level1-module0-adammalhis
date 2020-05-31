b0775782873547f469fa50021732397c4e7f5f83
root <root@72b18c74b3ec>
2020-05-31 00:30:08 +0000

    I did Houses today

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,75 @@
 package _01_houses;
 
+import java.awt.Color;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
-	public void run() {
+
+	static Robot Bob = new Robot();
+	
+	public static void main(String[] args) {
+	
+		Bob.setSpeed(50);
+		Bob.turn(270);
+		Bob.move(435);
+		Bob.turn(270);
+		Bob.move(250);
+		Bob.turn(180);
+		
+		for (int i=0; i<11; i++) {
+		Random r= new Random();
+		int red= r.nextInt(255);
+		int green= r.nextInt(255);
+		int blue= r.nextInt(255);
+			Color c= new Color(red, green, blue);
+			drawHouse(i*50, c);
+	
+		}
+	
+	}
+	public static void drawHouse(int size, Color theColor) {
+		
+		Bob.setPenColor(theColor);
+		Bob.penDown();
+		Bob.move(size);
+		
+		if (size < 250) {
+			drawDiagnalRoof();
+		}
+		else {
+			drawStraightRoof();
+		}
+		Bob.move(size);
+		Bob.turn(270);
+		Bob.setPenColor(0,255,0);
+		Bob.move(40);
+		Bob.turn(270);
 		
 	}
-}
+	public static void drawDiagnalRoof() {
+	
+		Bob.turn(45);
+		Bob.move(40);
+		Bob.turn(90);
+		Bob.move(40);
+		Bob.turn(45);
+	
+	
+	}
+	
+	public static void drawStraightRoof() {
+		
+		Bob.turn(90);
+		Bob.move(40);
+		Bob.turn(90);
+	
+		
+	}
+	
+	}
+
+
+
+

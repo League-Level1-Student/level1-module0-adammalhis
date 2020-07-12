f8ad9b9f74b65d07defd29bef20286ecfdb89bde
root <root@203da0ba9a05>
2020-07-12 19:57:38 +0000

    I did some of Cuteness Tv and Jack in the box today

--- src/_07_fortune_cookie/FortineCookieRunner.java
+++ src/_07_fortune_cookie/FortineCookieRunner.java
@@ -2,7 +2,7 @@ package _07_fortune_cookie;
 
 public class FortineCookieRunner {
 	public static void main(String[] args) {
-	new FortineCookie() .showButton();
+	new FortineCookie().showButton();
 	
 	}
 		
